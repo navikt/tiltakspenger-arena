5a9415fe9645e7efd13a35b038634facfe22ac2f
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-03-21 15:22:15 +0100

    Lagt på manglende ContentNegotiation

--- src/main/kotlin/no/nav/tiltakspenger/arena/TiltakApi.kt
+++ src/main/kotlin/no/nav/tiltakspenger/arena/TiltakApi.kt
@@ -1,10 +1,15 @@
 package no.nav.tiltakspenger.arena
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
 import io.ktor.server.auth.authenticate
 import io.ktor.server.config.ApplicationConfig
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.routing.routing
 import no.nav.security.token.support.v2.RequiredClaims
 import no.nav.security.token.support.v2.tokenValidationSupport
@@ -24,6 +29,13 @@ fun Application.tiltakApi(arenaOrdsClient: ArenaOrdsClient, config: ApplicationC
                 combineWithOr = false,
             ),
         )
+    }
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            registerModule(JavaTimeModule())
+            registerModule(KotlinModule.Builder().build())
+        }
     }
     routing {
         authenticate(issuerName) {
