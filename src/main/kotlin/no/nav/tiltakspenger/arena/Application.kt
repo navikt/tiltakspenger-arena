91cce59ed8dbd5d7cc036becfc07ce236b788503
Hågen Hasle <haagenhasle@gmail.com>
2023-07-03 14:33:40 +0200

    Installerer jul bridgen

--- src/main/kotlin/no/nav/tiltakspenger/arena/Application.kt
+++ src/main/kotlin/no/nav/tiltakspenger/arena/Application.kt
@@ -2,10 +2,13 @@ package no.nav.tiltakspenger.arena
 
 import io.ktor.server.config.ApplicationConfig
 import mu.KotlinLogging
+import org.slf4j.bridge.SLF4JBridgeHandler
 
 fun main() {
     System.setProperty("logback.configurationFile", "egenLogback.xml")
     System.setProperty("oracle.jdbc.fanEnabled", "false")
+    SLF4JBridgeHandler.removeHandlersForRootLogger()
+    SLF4JBridgeHandler.install()
 
     val log = KotlinLogging.logger {}
     val securelog = KotlinLogging.logger("tjenestekall")
