6569e9a1d7f98b1eeda93b3703df5b91a318303e
Benedicte Myrvoll <benemyr17@gmail.com>
2023-12-19 14:59:17 +0100

    F Legg til pre-commit med spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -162,3 +162,11 @@ tasks {
             .joinToString(separator = " ") { file -> "${file.name}" }
     }
 }
+
+task("addPreCommitGitHookOnBuild") {
+    println("⚈ ⚈ ⚈ Running Add Pre Commit Git Hook Script on Build ⚈ ⚈ ⚈")
+    exec {
+        commandLine("cp", "./.scripts/pre-commit", "./.git/hooks")
+    }
+    println("✅ Added Pre Commit Git Hook Script.")
+}
