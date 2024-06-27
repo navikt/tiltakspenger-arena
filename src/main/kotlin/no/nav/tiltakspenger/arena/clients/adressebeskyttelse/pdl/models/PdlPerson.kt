f8e8f33a0c08084b8253c5c9a142417ba9d0f2a6
Hågen Hasle <haagenhasle@gmail.com>
2024-06-27 20:14:34 +0200

    Fjernet klientkoden mot PDL og skjerming

--- src/main/kotlin/no/nav/tiltakspenger/arena/clients/adressebeskyttelse/pdl/models/PdlPerson.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.tiltakspenger.arena.clients.adressebeskyttelse.pdl.models
-
-data class PdlPerson(
-    val navn: List<Navn>,
-    val foedsel: List<Fødsel>,
-    val adressebeskyttelse: List<Adressebeskyttelse>,
-    val forelderBarnRelasjon: List<ForelderBarnRelasjon>,
-)
