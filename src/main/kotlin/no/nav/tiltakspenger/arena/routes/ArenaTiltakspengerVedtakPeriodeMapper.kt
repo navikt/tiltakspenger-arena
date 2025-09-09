3e1e8c05303eb1a9ec120cc8681d559f4519b7ff
Tia Firing <tia.firing@nav.no>
2025-09-09 14:09:40 +0200

    1602 nytt api som ikke filtrerer bort stansede vedtak

--- src/main/kotlin/no/nav/tiltakspenger/arena/routes/ArenaTiltakspengerVedtakPeriodeMapper.kt
+++ src/main/kotlin/no/nav/tiltakspenger/arena/routes/ArenaTiltakspengerVedtakPeriodeMapper.kt
@@ -1,31 +1,25 @@
 package no.nav.tiltakspenger.arena.routes
 
-import no.nav.tiltakspenger.arena.service.vedtakdetaljer.Rettighet
 import no.nav.tiltakspenger.arena.service.vedtakdetaljer.VedtakDetaljer
 import no.nav.tiltakspenger.libs.periodisering.Periodisering
 import java.time.LocalDate
 
 object ArenaTiltakspengerVedtakPeriodeMapper {
     fun Periodisering<VedtakDetaljer>?.toArenaTiltakspengerVedtakPeriode(): List<ArenaTiltakspengerVedtakPeriode> =
-        this?.filter {
-            it.verdi.rettighet == Rettighet.TILTAKSPENGER ||
-                it.verdi.rettighet == Rettighet.TILTAKSPENGER_OG_BARNETILLEGG ||
-                it.verdi.rettighet == Rettighet.BARNETILLEGG
-        }
-            ?.map {
-                ArenaTiltakspengerVedtakPeriode(
-                    fraOgMed = it.periode.fraOgMed,
-                    tilOgMed = it.periode.tilOgMed.toNullIfMax(),
-                    antallDager = it.verdi.antallDager,
-                    dagsatsTiltakspenger = it.verdi.dagsatsTiltakspenger,
-                    dagsatsBarnetillegg = it.verdi.dagsatsBarnetillegg,
-                    antallBarn = it.verdi.antallBarn,
-                    relaterteTiltak = it.verdi.tiltakGjennomføringsId,
-                    rettighet = it.verdi.rettighet,
-                    vedtakId = it.verdi.vedtakId,
-                    sakId = it.verdi.sakId,
-                )
-            }?.verdier ?: emptyList()
+        this?.map {
+            ArenaTiltakspengerVedtakPeriode(
+                fraOgMed = it.periode.fraOgMed,
+                tilOgMed = it.periode.tilOgMed.toNullIfMax(),
+                antallDager = it.verdi.antallDager,
+                dagsatsTiltakspenger = it.verdi.dagsatsTiltakspenger,
+                dagsatsBarnetillegg = it.verdi.dagsatsBarnetillegg,
+                antallBarn = it.verdi.antallBarn,
+                relaterteTiltak = it.verdi.tiltakGjennomføringsId,
+                rettighet = it.verdi.rettighet,
+                vedtakId = it.verdi.vedtakId,
+                sakId = it.verdi.sakId,
+            )
+        }?.verdier ?: emptyList()
 
     private fun LocalDate.toNullIfMax(): LocalDate? = if (this == LocalDate.MAX) {
         null
