e8e7026cfdeeff97a47886559b1fe232b746ede4
Henrik Solum <henrik.solum@nav.no>
2026-01-27 07:50:07 +0100

    1765 Deler registrert dato for anmerkninger med tidsinformasjon (#739)

--- src/main/kotlin/no/nav/tiltakspenger/arena/service/anmerkning/AnmerkningDetaljer.kt
+++ src/main/kotlin/no/nav/tiltakspenger/arena/service/anmerkning/AnmerkningDetaljer.kt
@@ -1,11 +1,11 @@
 package no.nav.tiltakspenger.arena.service.anmerkning
 
 import no.nav.tiltakspenger.arena.repository.anmerkning.ArenaAnmerkningDTO
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 class AnmerkningDetaljer(
     val kilde: String?,
-    val registrert: LocalDate?,
+    val registrert: LocalDateTime?,
     val beskrivelse: String?,
 )
 
