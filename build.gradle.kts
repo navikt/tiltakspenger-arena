6d6ef3b5d851fd3c1b81be875eeca30fa72fe8bb
John Andre Hestad <john.andre.hestad@nav.no>
2026-07-22 19:43:29 +0200

    Bump felleslibVersion to 0.0.20260722143244

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val jacksonAnnotationsVersion = "2.22"
 val mockkVersion = "1.14.11"
 val kotlinxCoroutinesVersion = "1.11.0"
 val testContainersVersion = "2.0.5"
-val felleslibVersion = "0.0.20260718174757"
+val felleslibVersion = "0.0.20260722143244"
 val kotestVersion = "6.2.2"
 
 plugins {
