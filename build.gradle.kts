6bfbc258039ecb1c66ff4a5a1495fd2ffb6d92e3
John Andre Hestad <john.andre.hestad@nav.no>
2026-09-07 22:34:56 +0200

    Oppdater tiltakspenger-libs til 0.0.20260907134256 tp-bau
    - Bumper felleslibVersion fra 0.0.20260904123939 til 0.0.20260907134256, som tar med
      oppdaterte tredjepartsversjoner og sikkerhetsfikser fra katalogen og plattform-BOM-en.
    - Verifisert med clean build (--no-build-cache): BUILD SUCCESSFUL, 68 tester, koverVerify
      og Konsist-testene grønne.
    - trivy rootfs mot build/install/tiltakspenger-arena/lib gir 0 sårbarheter i 90 jar-er.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val jacksonAnnotationsVersion = "2.22"
 val mockkVersion = "1.14.11"
 val kotlinxCoroutinesVersion = "1.11.0"
 val testContainersVersion = "2.0.5"
-val felleslibVersion = "0.0.20260904123939"
+val felleslibVersion = "0.0.20260907134256"
 val kotestVersion = "6.2.4"
 
 plugins {
